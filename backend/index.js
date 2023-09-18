88282b18b3bbcd758383fa4b0ab5cf9b67d0faf4
umairahmed88 <umairahmed_88@ymail.com>
2023-09-18 12:39:57 +0500

    Book Store MERN App

---
+++ backend/index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import booksRoute from "./routes/booksRoutes.js";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+// app.use(
+// 	cors({
+// 		origin: "http://localhost:3000",
+// 		method: ["GET", "POST", "PUT", "DELETE"],
+// 		allowedHeaders: ["Content-Type"],
+// 	})
+// );
+
+app.get("/", (request, response) => {
+	console.log(request);
+	return response.status(234).send("Welcome to MERN Stack");
+});
+
+app.use("/books", booksRoute);
+
+mongoose
+	.connect(mongoDBURL)
+	.then(() => {
+		console.log("App connected to database");
+		app.listen(PORT, () => {
+			console.log(`App is listening on ${PORT}`);
+		});
+	})
+	.catch((error) => {
+		console.log(error);
+	});
